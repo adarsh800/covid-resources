9bab81289efedb00d693ac61c619f3b3af235198
Adarsh Shukla <78147487+adarsh800@users.noreply.github.com>
2022-01-14 23:41:40 +0530

    Update settings.py

--- covid_resources/settings.py
+++ covid_resources/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = [config('ALLOWED_HOSTS_1')]
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS_1'), config('ALLOWED_HOST_2'), config('ALLOWED_HOST_3')]
 
 
 # Application definition
