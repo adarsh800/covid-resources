74c55ac9c920ad4bc94af8b047b356603e8af41a
Adarsh Shukla <78147487+adarsh800@users.noreply.github.com>
2022-01-15 12:46:49 +0530

    Update settings.py

--- covid_resources/settings.py
+++ covid_resources/settings.py
@@ -61,7 +61,6 @@ WSGI_APPLICATION = 'covid_resources.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -72,7 +71,6 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -100,7 +98,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_DIRS = [os.path.join(BASE_DIR,'covid_resources/static')]
@@ -108,7 +105,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
