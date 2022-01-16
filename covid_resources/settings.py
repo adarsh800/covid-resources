44c7db2637585c478d810be16d357682d472aaba
Adarsh Shukla <78147487+adarsh800@users.noreply.github.com>
2022-01-16 20:54:02 +0530

    Update settings.py

--- covid_resources/settings.py
+++ covid_resources/settings.py
@@ -109,6 +109,6 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-if DEBUG == True:
+if DEBUG == False:
     import django_heroku
     django_heroku.settings(locals())
